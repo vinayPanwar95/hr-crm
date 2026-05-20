430fdb8fa92f5810f2aab0a57baa4a7cc040b6a1
Vinay <vinay.panwar1604@gmail.com>
2026-05-20 12:18:59 +0800

    initial Commit

---
+++ src/main/java/com/fms/hr_crm/HrCrmApplication.java
@@ -0,0 +1,13 @@
+package com.fms.hr_crm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrCrmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrCrmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fms/hr_crm/HrCrmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fms.hr_crm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrCrmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
